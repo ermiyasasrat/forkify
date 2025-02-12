903703e6d4f4770370649228bd983adf3876d150
ermiyasasrat <ermiyasasrat580@gmail.com>
2025-02-12 12:52:10 +0300

    GET https://forkify-ermiyas.netlify.app/icons.c5b0f01c.svg.svg net::ERR_ABORTED 404 (Not Found) solution

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -3,7 +3,7 @@ import icons from 'url:../../img/icons.svg';
 
 class BookmarksView extends view {
   _parentElement = document.querySelector('.bookmarks__list');
-  _errorMessage = 'No bookmarks yet,Find a nice recipe and book mark it';
+  _errorMessage = 'No bookmarks yet, Find a nice recipe and bookmark it';
   _message = '';
 
   constructor() {
@@ -33,26 +33,24 @@ class BookmarksView extends view {
   _generateMarkupPreview(result) {
     const id = window.location.hash.slice(1);
     return ` 
-  <li class="preview">
-    <a class="preview__link ${
-      result.id === id ? 'preview__link--active' : ' '
-    } " href=${result.id}>
-        <figure class="preview__fig">
-              <img src="${result.image}" alt="Test" />
-        </figure>
-            <div class="preview__data">
-              <h4 class="preview__title">${result.title}</h4>
-              <p class="preview__publisher">${result.publisher}</p>
-                <div class="preview__user-generated ${
-                  result.key ? '' : 'hidden'
-                }">
-                <svg>
-              <use href="${icons}#icon-user"></use>
-                </svg>
-             </div>
-           </div>
-         </a>
- </li>`;
+      <li class="preview">
+        <a class="preview__link ${
+          result.id === id ? 'preview__link--active' : ''
+        }" href="#${result.id}">
+          <figure class="preview__fig">
+            <img src="${result.image}" alt="${result.title}" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${result.title}</h4>
+            <p class="preview__publisher">${result.publisher}</p>
+            <div class="preview__user-generated ${result.key ? '' : 'hidden'}">
+              <svg>
+                <use href="${icons}#icon-user"></use>
+              </svg>
+            </div>
+          </div>
+        </a>
+      </li>`;
   }
 }
 
