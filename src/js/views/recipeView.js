e532947ef163c3682ee5c41e2e3b270015068ae1
ermiyasasrat <ermiyasasrat580@gmail.com>
2025-02-12 13:35:06 +0300

    bookmark display issues

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -84,7 +84,7 @@ class RecipeView extends View {
           </div>
               <button class="btn--round btn--bookmark">
                 <svg class="">
-                  <use href="${icons}#icon-bookmark${
+                  <use href="${icons}#icon-bookmark ${
       this._data.bookmarked ? '-fill' : ''
     }"></use>
                 </svg>
